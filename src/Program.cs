ed42932183a9138feed664daa4cba1085060615c
James Sampica <jtsampica@gmail.com>
2023-12-01 23:42:16 -0600

    moving some stuff around

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,4 @@
-using TodoList.Components;
+using TodoList.Features.Shared;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +8,7 @@ builder.Services.AddRazorComponents();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
