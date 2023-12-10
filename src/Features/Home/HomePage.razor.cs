b5038f5eabcd09fc1030f286913e9f411acf3f5d
James Sampica <jtsampica@gmail.com>
2023-12-10 10:49:46 -0600

    Messing around with this pattern

---
+++ src/Features/Home/HomePage.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using TodoList.Routing;
+
+namespace TodoList.Features.Home;
+
+public partial class HomePage : ComponentBase, IRouteDefinition
+{
+    protected override Task OnInitializedAsync()
+    {
+        return base.OnInitializedAsync();
+    }
+
+    public override Task SetParametersAsync(ParameterView parameters)
+    {
+        return base.SetParametersAsync(parameters);
+    }
+
+    public void MapRoutes(WebApplication app)
+    {
+        app.MapGet("/route", Route);
+    }
+
+    public IResult Route()
+    {
+        return Results.Ok();
+    }
+}
